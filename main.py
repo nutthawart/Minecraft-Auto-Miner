0c34005e62373d4655abc358bdee9bec630274a4
nutthawart <117584069+nutthawart@users.noreply.github.com>
2023-12-10 08:39:41 +0700

    Add slots, messages and emojis

--- main.py
+++ main.py
@@ -2,29 +2,22 @@ from pyautogui import *
 import pyautogui as pg
 import time
 
-# Mining button key bind: M
-
 # intro
-print("*" * 20)
-print("Please change your keybind of Attack/Destroy to M.")
-print("ESC -> Options -> Controls -> Key Binds")
+print("*" * 60)
+print("[💡] ESC -> Options -> Controls -> Key Binds")
 while True:
-    continue_response = input("Continue...? (Y/N): ")
-    continue_response = continue_response.upper()
-    if continue_response == "Y":
+    key_bind = input("[🙋] Please enter your preferred keybind for Attack/Destroy : ")
+    if len(key_bind) == 1: #accept 1 len key to be hold
         break
-    elif continue_response == "N":
-        print("Don't worry! Take your time.")
-        time.sleep(3)
     else:
-        print("Invalid response prompt. Please try again.")
+        print("[❌] Invalid keybind. Please try again.")
         time.sleep(3)
 
 # type
-print("*" * 20)
-print("What type of pickaxes are you going to use this time?")
+print("*" * 60)
+print("[🙋] What type of pickaxes are you going to use this time?")
 while True:
-    pickaxe_type = input("(Wooden, Stone, Iron, Golden, Diamond, Netherite): ")
+    pickaxe_type = input("[💡] (Wooden, Stone, Iron, Golden, Diamond, Netherite): ")
     pickaxe_type = pickaxe_type.upper()
     if pickaxe_type == "WOODEN":
         seconds = 200
@@ -45,54 +38,57 @@ while True:
         seconds = 1
         break
     else:
-        print("Invalid pickaxe type. Please try again.")
+        print("[❌] Invalid pickaxe type. Please try again.")
         time.sleep(3)
 
 # slots
-print("*" * 20)
-print("How many slots do you have for pickaxes?")
+print("*" * 60)
+print("[💡] How many slots do you have for pickaxes?")
 while True:
-    slots = int(input("Enter a number between 0 and 9: "))
+    slots = int(input("[🙋] Enter a number between 0 and 9: "))
     if slots == 0:
-        print("I think you should go craft pickaxe.")
+        print("[❌] I think you should go craft pickaxe.")
         break
     elif 0 < slots <= 9:
         pickaxe_slots = slots
         break
     else:
-        print("Invalid number of slots. Please try again.")
+        print("[❌] Invalid number of slots. Please try again.")
+        time.sleep(3)
 
 
 # auto func
-def mine_for_seconds(secs):
-    pg.keyDown("M")
+def mine_for_seconds(secs, keybind):
+    pg.keyDown(keybind)
     sleep(secs)
-    pg.keyUp("M")
+    pg.keyUp(keybind)
+
+def mine_prompt(secs, keybind):
+    for i in range(1, pickaxe_slots+1):
+        pg.press(str(i)) #press 1-9 buttons
+        mine_for_seconds(secs, keybind)
+        print(f"[✅] {i} pickaxe has been used.")
 
 
 # summarize
-print("*" * 20)
-print(f"Pickaxe Type : {pickaxe_type}\nSlots : {pickaxe_slots}")
-print("After confirming the bot, Please enter the minecraft immediately")
+print("*" * 60)
+print(f"[🔑] Keybind : {key_bind}\n[⛏️]Pickaxe Type : {pickaxe_type}\n[🎒]Slots : {pickaxe_slots}")
+print("[⚠️] After confirming the bot, Please enter the game immediately")
 while True:
     ready_response = input("Ready?...? (Y/N): ")
     if ready_response == "Y":
-        print("The bot starting in 3...")
+        print("[⏱️] The bot starting in 3...")
         time.sleep(1)
-        print("The bot starting in 2...")
+        print("[⏱️] The bot starting in 2...")
         time.sleep(1)
-        print("The bot starting in 1...")
+        print("[⏱️] The bot starting in 1...")
         time.sleep(1)
-        print("The bot starting now!")
-        mine_for_seconds(seconds)
+        print("[🎉] The bot starting now!")
+        mine_prompt(seconds, key_bind)
         break
     elif ready_response == "N":
-        print("Don't worry! Take your time.")
+        print("[o] Don't worry! Take your time.")
         time.sleep(3)
     else:
-        print("Invalid response prompt. Please try again.")
+        print("[-] Invalid response prompt. Please try again.")
         time.sleep(3)
-
-
-"""sleep(3)
-mine_for_seconds(197)  # around 3:17 (1 block)"""
