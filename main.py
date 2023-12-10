bb52da5ac5b5c7f49838a2489bbc5766363800b0
nutthawart <117584069+nutthawart@users.noreply.github.com>
2023-12-10 18:18:04 +0700

    Update main.py
    - code was refactored into functions
    - dictionary to map pickaxe types to their corresponding durations
    - main function added that calls all the other functions in the correct order
    - print statement was added to indicate that a pickaxe has been picked up before it is used

--- main.py
+++ main.py
@@ -2,60 +2,54 @@ from pyautogui import *
 import pyautogui as pg
 import time
 
+PICKAXE_TYPES = {
+    "WOODEN": 191,
+    "STONE": 198,
+    "IRON": 376,
+    "GOLDEN": 49
+}
+
 # intro
-print("*" * 60)
-print("[💡] ESC -> Options -> Controls -> Key Binds")
-while True:
-    key_bind = input("[🙋] Please enter your preferred keybind for Attack/Destroy : ")
-    if len(key_bind) == 1: #accept 1 len key to be hold
-        break
-    else:
-        print("[❌] Invalid keybind. Please try again.")
-        time.sleep(3)
+def print_intro():
+    print("*" * 60)
+    print("[💡] ESC -> Options -> Controls -> Key Binds")
+
+# key
+def get_key_bind():
+    while True:
+        key_bind = input("[🙋] Please enter your preferred keybind for Attack/Destroy : ")
+        if len(key_bind) == 1: 
+            return key_bind
+        else:
+            print("[❌] Invalid keybind. Please try again.")
+            time.sleep(3)
 
 # type
-print("*" * 60)
-print("[🙋] What type of pickaxes are you going to use this time?")
-while True:
-    pickaxe_type = input("[💡] (Wooden, Stone, Iron, Golden, Diamond, Netherite): ")
-    pickaxe_type = pickaxe_type.upper()
-    if pickaxe_type == "WOODEN":
-        seconds = 200
-        break
-    elif pickaxe_type == "STONE":
-        seconds = 198
-        break
-    elif pickaxe_type == "IRON":
-        seconds = 4
-        break
-    elif pickaxe_type == "GOLDEN":
-        seconds = 3
-        break
-    elif pickaxe_type == "DIAMOND":
-        seconds = 2
-        break
-    elif pickaxe_type == "NETHERITE":
-        seconds = 1
-        break
-    else:
-        print("[❌] Invalid pickaxe type. Please try again.")
-        time.sleep(3)
+def get_pickaxe_type():
+    print("*" * 60)
+    print("[🙋] What type of pickaxes are you going to use this time?")
+    while True:
+        pickaxe_type = input("[💡] (Wooden, Stone, Iron, Golden, Diamond, Netherite): ").upper()
+        if pickaxe_type in PICKAXE_TYPES:
+            return pickaxe_type, PICKAXE_TYPES[pickaxe_type]
+        else:
+            print("[❌] Invalid pickaxe type. Please try again.")
+            time.sleep(3)
 
 # slots
-print("*" * 60)
-print("[💡] How many slots do you have for pickaxes?")
-while True:
-    slots = int(input("[🙋] Enter a number between 0 and 9: "))
-    if slots == 0:
-        print("[❌] I think you should go craft pickaxe.")
-        break
-    elif 0 < slots <= 9:
-        pickaxe_slots = slots
-        break
-    else:
-        print("[❌] Invalid number of slots. Please try again.")
-        time.sleep(3)
-
+def get_slots():
+    print("*" * 60)
+    print("[💡] How many slots do you have for pickaxes?")
+    while True:
+        slots = int(input("[🙋] Enter a number between 0 and 9: "))
+        if slots == 0:
+            print("[❌] I think you should go craft pickaxe.")
+            break
+        elif 0 < slots <= 9:
+            return slots
+        else:
+            print("[❌] Invalid number of slots. Please try again.")
+            time.sleep(3)
 
 # auto func
 def mine_for_seconds(secs, keybind):
@@ -63,32 +57,44 @@ def mine_for_seconds(secs, keybind):
     sleep(secs)
     pg.keyUp(keybind)
 
-def mine_prompt(secs, keybind):
+def mine_prompt(secs, keybind, pickaxe_slots):
     for i in range(1, pickaxe_slots+1):
-        pg.press(str(i)) #press 1-9 buttons
+        pg.press(str(i))
+        print(f"[🔄] {i} has been picked up.")
         mine_for_seconds(secs, keybind)
         print(f"[✅] {i} pickaxe has been used.")
 
-
 # summarize
-print("*" * 60)
-print(f"[🔑] Keybind : {key_bind}\n[⛏️]Pickaxe Type : {pickaxe_type}\n[🎒]Slots : {pickaxe_slots}")
-print("[⚠️] After confirming the bot, Please enter the game immediately")
-while True:
-    ready_response = input("Ready?...? (Y/N): ")
-    if ready_response == "Y":
-        print("[⏱️] The bot starting in 3...")
-        time.sleep(1)
-        print("[⏱️] The bot starting in 2...")
-        time.sleep(1)
-        print("[⏱️] The bot starting in 1...")
-        time.sleep(1)
-        print("[🎉] The bot starting now!")
-        mine_prompt(seconds, key_bind)
-        break
-    elif ready_response == "N":
-        print("[o] Don't worry! Take your time.")
-        time.sleep(3)
-    else:
-        print("[-] Invalid response prompt. Please try again.")
-        time.sleep(3)
+def summarize_and_start(key_bind, pickaxe_type, pickaxe_slots, seconds):
+    print("*" * 60)
+    print(f"[🔑] Keybind : {key_bind}\n[⛏️]Pickaxe Type : {pickaxe_type}\n[🎒]Slots : {pickaxe_slots}")
+    print("[⚠️] After confirming the bot, Please enter the game immediately")
+    while True:
+        ready_response = input("Ready?...? (Y/N): ").upper()
+        if ready_response == "Y":
+            print("[⏱️] The bot starting in 3...")
+            time.sleep(1)
+            print("[⏱️] The bot starting in 2...")
+            time.sleep(1)
+            print("[⏱️] The bot starting in 1...")
+            time.sleep(1)
+            print("[🎉] The bot starting now!")
+            mine_prompt(seconds, key_bind, pickaxe_slots)
+            break
+        elif ready_response == "N":
+            print("[o] Don't worry! Take your time.")
+            time.sleep(3)
+        else:
+            print("[-] Invalid response prompt. Please try again.")
+            time.sleep(3)
+
+# main
+def main():
+    print_intro()
+    key_bind = get_key_bind()
+    pickaxe_type, seconds = get_pickaxe_type()
+    pickaxe_slots = get_slots()
+    summarize_and_start(key_bind, pickaxe_type, pickaxe_slots, seconds)
+
+if __name__ == "__main__":
+    main()
